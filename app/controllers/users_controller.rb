ae12b210a7a3b9984616958cef18d06d6caaf733
EC2 Default User <129806087+kano0000@users.noreply.github.com>
2023-12-16 06:19:17 +0000

    [Update]validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
   def index
     @users = User.all
     @book = Book.new
@@ -27,6 +28,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:introduction)
+  end
+  
+  def ensure_correct_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 
 end
