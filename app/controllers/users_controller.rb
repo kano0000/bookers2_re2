eef7acee9aff5f96676679822e9870beeb054f64
EC2 Default User <129806087+kano0000@users.noreply.github.com>
2023-12-16 06:56:44 +0000

    [finish]課題３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
+  
   def index
     @users = User.all
     @book = Book.new
@@ -27,7 +28,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:introduction)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
   
   def ensure_correct_user
