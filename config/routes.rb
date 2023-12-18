1cc2e7fa3ffc52b21c3e3121adecf97f3d7a7fe0
EC2 Default User <129806087+kano0000@users.noreply.github.com>
2023-12-18 12:34:35 +0000

    [Update]DM

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  
   root to: 'homes#top'
   get 'home/about' => "homes#about"
+  
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
@@ -13,6 +15,9 @@ Rails.application.routes.draw do
       get "followings" => "relationships#followings", as: "followings"
       get "followers" => "relationships#followers", as: "followers"
   end
+  
+  resources :chats, only: [:show, :create, :destroy]
+  
   get "/search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
