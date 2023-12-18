758794d79f7e0700bf51c08e7fc3811514e12abe
EC2 Default User <129806087+kano0000@users.noreply.github.com>
2023-12-18 08:34:36 +0000

    [Update]閲覧数

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_17_061914) do
+ActiveRecord::Schema.define(version: 2023_12_18_082317) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_12_17_061914) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "view_count"
   end
 
   create_table "favorites", force: :cascade do |t|
